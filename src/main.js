8b2bb16f1e2042c715345f920ad7c5e334b588d2
xu <345632369@qq.com>
2018-06-20 16:45:13 +0800

    引入we-vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+//引入we-vue
+import WeVue from 'we-vue'
+import 'we-vue/lib/style.css'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+
+//引入we-vue
+Vue.use(WeVue)
 
 /* eslint-disable no-new */
 new Vue({
